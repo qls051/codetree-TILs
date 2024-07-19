cf508abddd99f3e6d2c5cd85e28e85bea8ad1195
qls051 <123608003+qls051@users.noreply.github.com>
2024-07-19 09:00:18 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/장학금/scholarship.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a>> b;
+
+    if(a >= 90 && b >= 95){
+        cout << 100000;
+    }
+    else{
+        cout << 0;
+    } 
+    return 0;
+}
